8da2f7e86482510ee32304cb1bc8df95f21a12e9
Lukylix <lucas.garcia.bobi@gmail.com>
2022-06-03 18:00:38 +0200

    Simplify Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,31 @@
-import React, { createContext, useContext, useState, useMemo, useEffect } from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 
 import { filterChildrenByType } from "../../utils";
 
 import styles from "./Modal.css";
 
-const ModalContext = createContext();
+// const ModalContext = createContext();
 
-export function ModalOpen({ children, onClick, ...args }) {
-  const { setIsVisible } = useContext(ModalContext);
-  return (
-    <div style={{ width: "fit-content" }} {...args} onClick={() => setIsVisible(true) && onClick?.()}>
-      {children}
-    </div>
-  );
-}
-
-Modal.propTypes = {
-  children: PropTypes.node,
-  onClick: PropTypes.func,
-};
-
-export function Modal({ children, buttonProps, displayCloseButton = true, _type = "Modal", ...args }) {
-  const { isVisible, setIsVisible } = useContext(ModalContext);
+export function Modal({
+  isVisible,
+  setIsVisible,
+  children,
+  buttonProps: { onClick: onClickButton, ...buttonProps } = {},
+  displayCloseButton = true,
+  _type = "Modal",
+  ...args
+}) {
   return (
     <>
       {isVisible && (
         <div className={styles.modalContent} {...args}>
           {displayCloseButton && (
-            <button className={styles.closeButton} {...buttonProps} onClick={() => setIsVisible(false)}>
+            <button
+              className={styles.closeButton}
+              {...buttonProps}
+              onClick={() => setIsVisible?.(false) && onClickButton?.()}
+            >
               x
             </button>
           )}
@@ -49,25 +46,13 @@ Modal.defaultProps = {
   displayCloseButton: true,
 };
 
-export function ModalProvider({ children, isVisible = false, setIsVisible = () => {}, onClose = () => {} }) {
-  const [internalIsVisible, setInternalIsVisible] = useState(isVisible || false);
-
-  useEffect(() => {
-    if (isVisible !== undefined && isVisible !== internalIsVisible) setInternalIsVisible(isVisible);
-  }, [isVisible]);
-
-  const setIsVisibleBoth = (isVisible) => {
-    setInternalIsVisible(isVisible);
-    setIsVisible(isVisible);
-    if (!isVisible) onClose();
-  };
-
+export function ModalProvider({ children }) {
   const [modalChildren, otherChildren] = useMemo(() => filterChildrenByType(children, ["Modal"]), [children]);
 
   return (
-    <ModalContext.Provider value={{ isVisible: internalIsVisible, setIsVisible: setIsVisibleBoth }}>
+    <>
       {modalChildren}
       {otherChildren}
-    </ModalContext.Provider>
+    </>
   );
 }
--- src/stories/Modal.stories.js
+++ src/stories/Modal.stories.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { ModalProvider, Modal, ModalOpen } from "../../dist/esm/index.js";
+import { ModalProvider, Modal } from "../../dist/esm/index.js";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -9,26 +9,11 @@ export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
 
-export const WithCounpoundComposant = (args) => {
-  return (
-    <ModalProvider {...args}>
-      <Modal>
-        <h1>Modal Content</h1>
-      </Modal>
-      <h2>With Compound Composant</h2>
-      <p>Some random content</p>
-      <ModalOpen>
-        <button>Open Modal</button>
-      </ModalOpen>
-    </ModalProvider>
-  );
-};
-
-export const WithoutModalOpenComposant = (args) => {
+export const Default = (args) => {
   const [isVisible, setIsVisible] = useState(false);
   return (
-    <ModalProvider isVisible={isVisible} setIsVisible={setIsVisible} {...args}>
-      <Modal>
+    <ModalProvider>
+      <Modal isVisible={isVisible} setIsVisible={setIsVisible}>
         <h1>Modal Content</h1>
       </Modal>
       <h2>Whitout ModalOpen Composant</h2>
