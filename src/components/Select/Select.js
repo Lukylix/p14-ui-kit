f31d463f694c9e9a998b9d3ef54c2fedefc25b08
Lukylix <lucas.garcia.bobi@gmail.com>
2022-06-04 13:49:35 +0200

    Keyboard Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -17,8 +17,13 @@ export default function Select({
 }) {
   const [isVisible, setIsVisible] = useState(false);
   const [inputLabel, setInputLabel] = useState(value);
+  const [hoverIndex, setHoverIndex] = useState(null);
+  const inputRef = useRef();
+  const optionsRef = useRef();
+  useOutsideClick([inputRef, optionsRef], () => setIsVisible(false) && setHoverIndex(null));
+
   const fileredOptions = useMemo(() => {
-    if (!isSearchable) return options;
+    if (!isSearchable || !inputLabel) return options;
     return options.filter((option) => option.label.toLowerCase().includes(inputLabel.toLowerCase()));
   }, [options, inputLabel, isSearchable]);
 
@@ -28,9 +33,37 @@ export default function Select({
     if (value !== inputValue) setInputLabel(valueLabel);
   }, [value]);
 
-  const inputRef = useRef();
-  const optionsRef = useRef();
-  useOutsideClick([inputRef, optionsRef], () => setIsVisible(false));
+  const handleKeyDown = (event) => {
+    console.log(hoverIndex);
+    switch (event.code) {
+      case "ArrowDown":
+        if (hoverIndex === null) return setHoverIndex(0);
+        setHoverIndex((prevHoverIndex) => (prevHoverIndex + 1 > fileredOptions.length - 1 ? 0 : prevHoverIndex + 1));
+        break;
+      case "ArrowUp":
+        if (hoverIndex === null) return setHoverIndex(fileredOptions.length - 1);
+        setHoverIndex((prevHoverIndex) => (prevHoverIndex - 1 < 0 ? fileredOptions.length - 1 : prevHoverIndex - 1));
+        break;
+      case "Enter":
+        if (hoverIndex === null) setHoverIndex(0);
+
+        setInputLabel(fileredOptions[hoverIndex].label);
+        onChange(fileredOptions[hoverIndex].value);
+        setHoverIndex(null);
+        setIsVisible(false);
+        break;
+      case "Tab":
+        if (hoverIndex !== null) {
+          setInputLabel(fileredOptions[hoverIndex].label);
+          onChange(fileredOptions[hoverIndex].value);
+          setHoverIndex(null);
+        }
+        setIsVisible(false);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className={`${styles.select} ${isVisible ? styles.active : ""}`} styles={customStyles.container}>
@@ -38,26 +71,30 @@ export default function Select({
         ref={inputRef}
         name={name}
         onClick={() => setIsVisible(true)}
+        onFocus={() => setIsVisible(true)}
         className={styles.input}
         value={inputLabel}
         placeholder={placeHolder}
         readOnly={!isSearchable}
         style={customStyles?.input}
-        onChange={(e) => setInputLabel(e.target.value)}
+        onChange={(e) => setInputLabel(e.target.value) && setHoverIndex(0)}
+        onKeyDown={handleKeyDown}
       />
       <ChevronDown height="1em" />
       {isVisible && (
-        <div ref={optionsRef} className={styles.selectOptions} style={customStyles.options}>
+        <div ref={optionsRef} className={styles.selectOptions} style={customStyles.options} onKeyDown={handleKeyDown}>
           {fileredOptions.map((option, index) => (
             <div
               key={index}
-              className={styles.selectOption}
+              className={styles.selectOption + (index === hoverIndex ? ` ${styles.hover}` : "")}
               style={customStyles.option}
               onClick={() => {
                 setIsVisible(false);
                 onChange?.(option?.value);
                 setInputLabel(option?.label);
+                setHoverIndex(null);
               }}
+              onMouseEnter={() => setHoverIndex(index)}
             >
               {option?.label}
             </div>
